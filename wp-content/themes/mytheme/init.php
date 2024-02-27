5de2957d77c768a0a193b55f3e3c0f1c3d58536d
iman656 <125255511+iman656@users.noreply.github.com>
2024-02-27 12:00:15 +0100

    modify 1 header

--- wp-content/themes/mytheme/init.php
+++ wp-content/themes/mytheme/init.php
@@ -3,11 +3,14 @@
 
 require_once('vite.php');
 require_once('ajax.php');
+require_once("settings.php");
+// require_once("shortcodes.php");
 
 function mytheme_enqueue()
 {
     $theme_directory = get_template_directory_uri();
     wp_enqueue_style("mystyle", $theme_directory . "/style.css");
+    
 }
 add_action("wp_enqueue_scripts", "mytheme_enqueue");
 
@@ -17,14 +20,14 @@ function mytheme_init()
     add_theme_support('post-thumbnails');
 
     // register MENU
-    $menu = array(
-        'main_menu' => 'main_menu',
-        'main_menu_icons' => 'main_menu_icons',
-        'footer_address' => 'footer_address',
-        'footer_links' => 'footer_links',
-        'footer_help' => 'footer_help',
-        'footer_newsletter' => 'footer_newsletter'
+    $menus = array(
+        'huvudmeny' => 'huvudmeny',
+        'menyikoner'=>'menyikoner',
+        'footer_meny' => 'footer_meny',
+        'footer_meny2' => 'footer_meny2',
+        'socialamedierikoner'=>'socialamedierikoner',
+     
     );
-    register_nav_menus($menu);
+    register_nav_menus($menus);
 }
 add_action("after_setup_theme", "mytheme_init");
