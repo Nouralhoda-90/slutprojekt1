f27a20401708dfd34096b02601e239b19a0de941
nouralhoda-90 <nouralhoda.ahmad@gritacademy.se>
2024-02-28 01:08:04 +0100

    d

--- wp-content/themes/mytheme/init.php
+++ wp-content/themes/mytheme/init.php
@@ -3,15 +3,12 @@
 
 require_once('vite.php');
 require_once('ajax.php');
-
-require_once("setting.php");
-// require_once("shortcodes.php");
+require_once('setting.php');
 
 function mytheme_enqueue()
 {
     $theme_directory = get_template_directory_uri();
     wp_enqueue_style("mystyle", $theme_directory . "/style.css");
-    
 }
 add_action("wp_enqueue_scripts", "mytheme_enqueue");
 
@@ -22,14 +19,14 @@ function mytheme_init()
     add_theme_support('post-thumbnails');
 
     // register MENU
-    $menus = array(
-        'huvudmeny' => 'huvudmeny',
-        'menyikoner'=>'menyikoner',
+    $menu = array(
+        'main_menu' => 'main_menu',
+        'main_menu_icons' => 'main_menu_icons',
         'footer_social_media' => 'footer_social_media',
         'footer_shopping' => 'footer_shopping',
         'footer_links' => 'footer_links',
         'footer_blog' => 'footer_blog'
     );
-    register_nav_menus($menus);
+    register_nav_menus($menu);
 }
 add_action("after_setup_theme", "mytheme_init");
