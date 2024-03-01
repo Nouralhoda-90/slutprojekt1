e06feb736623c0ecf3263780aa19fc9b675121d8
iman656 <125255511+iman656@users.noreply.github.com>
2024-03-01 14:25:51 +0100

    fix-header-serviceikon-footer-from noor

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -4,8 +4,11 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+//Initialize theme
 require_once(get_template_directory() . "/init.php");
-//test
+require_once("shortcodes.php");
+
+
 /**
  * support Woocommerce function
  */
@@ -23,6 +26,8 @@ function customize_checkout_button_text()
 {
     return 'Check Out';
 }
+
+
 
 function custom_div_shortcode( $atts, $content = null ) {
     
@@ -45,28 +50,5 @@ function custom_div_shortcode( $atts, $content = null ) {
     
 }
 add_shortcode( 'custom_div', 'custom_div_shortcode' );
-
-function display_advantages_shortcode() {
-    ob_start(); ?>
-
-    <div class="advantages">
-        <div class="advantage">
-            <span class="icon"><?php echo file_get_contents( get_template_directory_uri() . '/resources/images/shipping.svg' ); ?></span>
-            <span class="text">FREE SHIPPING</span>
-        </div>
-        <div class="advantage">
-            <span class="icon"><?php echo file_get_contents( get_template_directory_uri() . '/resources/images/money-back.svg' ); ?></span>
-            <span class="text">100% MONEY BACK</span>
-        </div>
-        <div class="advantage">
-            <span class="icon"><?php echo file_get_contents( get_template_directory_uri() . '/resources/images/support.svg' ); ?></span>
-            <span class="text">SUPPORT 24/7</span>
-        </div>
-    </div>
-    <?php
-    return ob_get_clean();
-}
-
-
 
 
--- wp-content/themes/mytheme/shortcodes.php
+++ wp-content/themes/mytheme/shortcodes.php
@@ -1,6 +1,22 @@
 <?php
-
-require_once("parts/advantages.php");
-
-
-add_shortcode('display_advantages', 'display_advantages_shortcode');
+function services_shortcode() {
+    ob_start();
+    ?>
+    <div class="services">
+        <div class="services-content">
+            <span class="services-icon"><?php echo file_get_contents(get_template_directory_uri() . '/resources/images/freeshipping.svg'); ?></span>
+            <span class="services-text">FREE SHIPPING</span>
+        </div>
+        <div class="services-content money-back">
+            <span class="services-icon"><?php echo file_get_contents(get_template_directory_uri() . '/resources/images/moneyback.svg'); ?></span>
+            <span class="services-text">100% MONEY BACK</span>
+        </div>
+        <div class="services-content">
+            <span class="services-icon"><?php echo file_get_contents(get_template_directory_uri() . '/resources/images/support.svg'); ?></span>
+            <span class="services-text">SUPPORT 24/7</span>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('services', 'services_shortcode');
