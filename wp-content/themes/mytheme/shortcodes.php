5a4a9b9b6d02aec2c4dab2f93a2f15898b259108
iman656 <125255511+iman656@users.noreply.github.com>
2024-03-02 03:35:17 +0100

    shortcodes

--- wp-content/themes/mytheme/shortcodes.php
+++ wp-content/themes/mytheme/shortcodes.php
@@ -43,3 +43,24 @@ function newsletter_shortcode() {
     return ob_get_clean();
 }
 add_shortcode('newsletter', 'newsletter_shortcode');
+
+
+//Shortcode-box till "bedsheet sets" i home
+function moody_studio_shortcode_draw_box($attr)
+{
+    $attr = shortcode_atts(
+        array(
+        ),
+        $attr,
+        "box"
+    );
+
+       return '<div class="box">
+       <h2>BEDSHEET SETS</h2>
+       <h3>300 kr</h3>
+       <h4>700 kr</h4>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+       </div>';
+
+}
+add_shortcode("box", "moody_studio_shortcode_draw_box");
